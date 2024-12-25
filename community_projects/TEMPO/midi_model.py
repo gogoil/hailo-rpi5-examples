10f854e8806d6a8fd1ac2ac0b5ca45fb46aaeb84
dorong <dorong@hailo.ai>
2024-12-26 01:43:49 +0200

    vs pipeline

--- community_projects/TEMPO/midi_model.py
+++ community_projects/TEMPO/midi_model.py
@@ -1,47 +1,49 @@
 from midi_tokenizer import MIDITokenizer
 import numpy as np
 import tqdm
-
+ 
 import numpy as np
-from hailo_platform import VDevice, HailoSchedulingAlgorithm, Device
+from hailo_platform import VDevice, HailoSchedulingAlgorithm, Device, ConfigureParams, HailoStreamInterface, InputVStreamParams, OutputVStreamParams, FormatType, InferVStreams
 import hailo_platform
-
+ 
 timeout_ms = 1000
-
-params = VDevice.create_params()
-params.scheduling_algorithm = HailoSchedulingAlgorithm.ROUND_ROBIN
-
+ 
+ 
 class MIDIModel:
     MAX_MIDI_SEQUENCE_LENGTH = 512
-
-
+ 
+ 
     def __init__(self, model_base, model_token, model_base_emb, model_token_emb):
         self.tokenizer = MIDITokenizer()
         self.tokenizer.set_optimise_midi()
-        
-        vdevice = VDevice(params)
-        self.net = vdevice.create_infer_model(model_base)
-        for model_input in self.net.inputs:
-            model_input.set_format_type(hailo_platform.FormatType.FLOAT32)
-        for output in self.net.outputs:
-            output.set_format_type(hailo_platform.FormatType.FLOAT32)
-        self.net = self.net.configure()
-
-        self.net_token = vdevice.create_infer_model(model_token)
-        for model_input in self.net_token.inputs:
-            model_input.set_format_type(hailo_platform.FormatType.FLOAT32)
-        for output in self.net_token.outputs:
-            output.set_format_type(hailo_platform.FormatType.FLOAT32)
-        self.net_token = self.net_token.configure()
-
+       
+        params = VDevice.create_params()
+        params.scheduling_algorithm = HailoSchedulingAlgorithm.ROUND_ROBIN
+        params.multi_process_service=False
+        self.vdevice = VDevice(params)
+ 
+        configure_params = ConfigureParams.create_from_hef(model_base, interface=HailoStreamInterface.INTEGRATED)
+        configure_params["model_base"].batch_size = 1
+        configure_token_params = ConfigureParams.create_from_hef(model_token, interface=HailoStreamInterface.INTEGRATED)
+        configure_token_params["model_token"].batch_size = 1
+ 
+        self.net = self.vdevice.configure(model_base, configure_params)[0]
+        self.net_token = self.vdevice.configure(model_token, configure_token_params)[0]
+ 
+        self.input_vstreams_params = InputVStreamParams.make(self.net, format_type=FormatType.FLOAT32)
+        self.input_vstreams_params["model_base/input_layer1"].user_buffer_format.type = FormatType.FLOAT32
+        self.output_vstreams_params = OutputVStreamParams.make(self.net, format_type=FormatType.FLOAT32)
+ 
+        self.input_vstreams_token_params = InputVStreamParams.make(self.net_token, format_type=FormatType.FLOAT32)
+        self.input_vstreams_token_params["model_token/input_layer1"].user_buffer_format.type = FormatType.FLOAT32
+        self.output_vstreams_token_params = OutputVStreamParams.make(self.net_token, format_type=FormatType.FLOAT32)
+ 
         self.net_emb = model_base_emb
         self.net_token_emb = model_token_emb
-        self.net_bindings = self.net.create_bindings()
-        self.net_token_bindings = self.net_token.create_bindings()
-
-    def forward_token(self, hidden_state, x=None):
+ 
+    def forward_token(self, infer_pipeline_token, hidden_state, x=None):
         """
-
+ 
         :param hidden_state: (batch_size, n_embd)
         :param x: (batch_size, token_sequence_length)
         :return: (batch_size, 1 + token_sequence_length, vocab_size)
@@ -56,15 +58,10 @@ class MIDIModel:
                        mode="constant", constant_values=self.tokenizer.pad_id)
         x = np.expand_dims(self.net_token_emb[x], 1)
         hidden_state = np.concatenate([hidden_state, x], axis=2)
-
-        self.net_token_bindings.input().set_buffer(hidden_state)
-        self.net_token_bindings.output().set_buffer(np.empty(hidden_state.shape()))
-        self.net_token.run([self.net_token_bindings], timeout_ms)
-        logits = self.net_token_bindings.output().get_buffer()
-        
+        logits = infer_pipeline_token.infer(hidden_state)        
         return logits[:, 0, :return_indexs]
-
-    def forward(self, x):
+ 
+    def forward(self, infer_pipeline, x):
         """
         :param x: (batch_size, midi_sequence_length, token_sequence_length)
         :return: hidden (batch_size, midi_sequence_length, n_embd)
@@ -77,20 +74,18 @@ class MIDIModel:
         # merge token sequence
         x = self.net_emb[x]
         x = np.expand_dims(np.sum(x, axis=-2), 1)
-
+ 
         shape = x.shape
-        self.net_bindings.input().set_buffer(x.reshape(-1, shape[1] * shape[2] // 8, 8, shape[-1]))
-        self.net_bindings.output().set_buffer(np.empty(x.shape))
-        self.net.run([self.net_bindings], timeout_ms)
-        hidden_state = self.net_bindings.output().get_buffer()
-        
+        x = x.reshape(-1, shape[1] * shape[2] // 8, 8, shape[-1])
+        hidden_state = infer_pipeline.infer(x)
+       
         return hidden_state[:, 0, :return_indexs]
-
+ 
     def softmax(self, x, axis):
         x_max = np.amax(x, axis=axis, keepdims=True)
         exp_x_shifted = np.exp(x - x_max)
         return exp_x_shifted / np.sum(exp_x_shifted, axis=axis, keepdims=True)
-
+ 
     def sample_top_p_k(self, probs, p, k, generator=None):
         if generator is None:
             generator = np.random
@@ -109,92 +104,94 @@ class MIDIModel:
         next_token = np.stack([generator.choice(idxs, p=pvals) for pvals, idxs in zip(probs_sort_flat, probs_idx_flat)])
         next_token = next_token.reshape(*shape[:-1])
         return next_token
-
+ 
     def generate(self, prompt=None, batch_size=1, max_len=512, temp=1.0, top_p=0.98, top_k=20,
                  disable_patch_change=False, disable_control_change=False, disable_channels=None, generator=None):
-        tokenizer = self.tokenizer
-        if disable_channels is not None:
-            disable_channels = [tokenizer.parameter_ids["channel"][c] for c in disable_channels]
-        else:
-            disable_channels = []
-        max_token_seq = tokenizer.max_token_seq
-        if prompt is None:
-            input_tensor = np.full((1, 1, max_token_seq), tokenizer.pad_id, dtype=np.int64)
-            input_tensor[0, 0, 0] = tokenizer.bos_id  # bos
-            input_tensor = np.repeat(input_tensor, repeats=batch_size, axis=0)
-        else:
-            if len(prompt.shape) == 2:
-                prompt = prompt[None, :]
-                prompt = np.repeat(prompt, repeats=batch_size, axis=0)
-            elif prompt.shape[0] == 1:
-                prompt = np.repeat(prompt, repeats=batch_size, axis=0)
-            elif len(prompt.shape) != 3 or prompt.shape[0] != batch_size:
-                raise ValueError(f"invalid shape for prompt, {prompt.shape}")
-            prompt = prompt[..., :max_token_seq]
-            if prompt.shape[-1] < max_token_seq:
-                prompt = np.pad(prompt, ((0, 0), (0, 0), (0, max_token_seq - prompt.shape[-1])),
-                                mode="constant", constant_values=tokenizer.pad_id)
-            input_tensor = prompt
-
-        cur_len = input_tensor.shape[1]
-        bar = tqdm.tqdm(desc="generating", total=max_len - cur_len)
-        with bar:
-            while cur_len < max_len:
-                end = [False] * batch_size
-                hidden = self.forward(input_tensor)[:, -1]
-                next_token_seq = None
-                event_names = [""] * batch_size
-                for i in range(max_token_seq):
-                    mask = np.zeros((batch_size, tokenizer.vocab_size), dtype=np.int64)
-                    for b in range(batch_size):
-                        if end[b]:
-                            mask[b, tokenizer.pad_id] = 1
-                            continue
-                        if i == 0:
-                            mask_ids = list(tokenizer.event_ids.values()) + [tokenizer.eos_id]
-                            if disable_patch_change:
-                                mask_ids.remove(tokenizer.event_ids["patch_change"])
-                            if disable_control_change:
-                                mask_ids.remove(tokenizer.event_ids["control_change"])
-                            mask[b, mask_ids] = 1
-                        else:
-                            param_names = tokenizer.events[event_names[b]]
-                            if i > len(param_names):
-                                mask[b, tokenizer.pad_id] = 1
-                                continue
-                            param_name = param_names[i - 1]
-                            mask_ids = tokenizer.parameter_ids[param_name]
-                            if param_name == "channel":
-                                mask_ids = [i for i in mask_ids if i not in disable_channels]
-                            mask[b, mask_ids] = 1
-                    mask = np.expand_dims(mask, 1)
-                    x = next_token_seq
-                    logits = self.forward_token(hidden, x)[:, -1:]
-                    scores = self.softmax(logits / temp, axis=-1) * mask
-                    samples = self.sample_top_p_k(scores, top_p, top_k, generator=generator)
-                    if i == 0:
-                        next_token_seq = samples
-                        for b in range(batch_size):
-                            if end[b]:
-                                continue
-                            eid = samples[b].item()
-                            if eid == tokenizer.eos_id:
-                                end[b] = True
+        with InferVStreams(self.net, self.input_vstreams_params, self.output_vstreams_params) as infer_pipeline:
+            with InferVStreams(self.net_token, self.input_vstreams_token_params, self.output_vstreams_token_params) as infer_pipeline_token:
+                tokenizer = self.tokenizer
+                if disable_channels is not None:
+                    disable_channels = [tokenizer.parameter_ids["channel"][c] for c in disable_channels]
+                else:
+                    disable_channels = []
+                max_token_seq = tokenizer.max_token_seq
+                if prompt is None:
+                    input_tensor = np.full((1, 1, max_token_seq), tokenizer.pad_id, dtype=np.int64)
+                    input_tensor[0, 0, 0] = tokenizer.bos_id  # bos
+                    input_tensor = np.repeat(input_tensor, repeats=batch_size, axis=0)
+                else:
+                    if len(prompt.shape) == 2:
+                        prompt = prompt[None, :]
+                        prompt = np.repeat(prompt, repeats=batch_size, axis=0)
+                    elif prompt.shape[0] == 1:
+                        prompt = np.repeat(prompt, repeats=batch_size, axis=0)
+                    elif len(prompt.shape) != 3 or prompt.shape[0] != batch_size:
+                        raise ValueError(f"invalid shape for prompt, {prompt.shape}")
+                    prompt = prompt[..., :max_token_seq]
+                    if prompt.shape[-1] < max_token_seq:
+                        prompt = np.pad(prompt, ((0, 0), (0, 0), (0, max_token_seq - prompt.shape[-1])),
+                                        mode="constant", constant_values=tokenizer.pad_id)
+                    input_tensor = prompt
+ 
+                cur_len = input_tensor.shape[1]
+                bar = tqdm.tqdm(desc="generating", total=max_len - cur_len)
+                with bar:
+                    while cur_len < max_len:
+                        end = [False] * batch_size
+                        hidden = self.forward(infer_pipeline, input_tensor)[:, -1]
+                        next_token_seq = None
+                        event_names = [""] * batch_size
+                        for i in range(max_token_seq):
+                            mask = np.zeros((batch_size, tokenizer.vocab_size), dtype=np.int64)
+                            for b in range(batch_size):
+                                if end[b]:
+                                    mask[b, tokenizer.pad_id] = 1
+                                    continue
+                                if i == 0:
+                                    mask_ids = list(tokenizer.event_ids.values()) + [tokenizer.eos_id]
+                                    if disable_patch_change:
+                                        mask_ids.remove(tokenizer.event_ids["patch_change"])
+                                    if disable_control_change:
+                                        mask_ids.remove(tokenizer.event_ids["control_change"])
+                                    mask[b, mask_ids] = 1
+                                else:
+                                    param_names = tokenizer.events[event_names[b]]
+                                    if i > len(param_names):
+                                        mask[b, tokenizer.pad_id] = 1
+                                        continue
+                                    param_name = param_names[i - 1]
+                                    mask_ids = tokenizer.parameter_ids[param_name]
+                                    if param_name == "channel":
+                                        mask_ids = [i for i in mask_ids if i not in disable_channels]
+                                    mask[b, mask_ids] = 1
+                            mask = np.expand_dims(mask, 1)
+                            x = next_token_seq
+                            logits = self.forward_token(infer_pipeline_token, hidden, x)[:, -1:]
+                            scores = self.softmax(logits / temp, axis=-1) * mask
+                            samples = self.sample_top_p_k(scores, top_p, top_k, generator=generator)
+                            if i == 0:
+                                next_token_seq = samples
+                                for b in range(batch_size):
+                                    if end[b]:
+                                        continue
+                                    eid = samples[b].item()
+                                    if eid == tokenizer.eos_id:
+                                        end[b] = True
+                                    else:
+                                        event_names[b] = tokenizer.id_events[eid]
                             else:
-                                event_names[b] = tokenizer.id_events[eid]
-                    else:
-                        next_token_seq = np.concatenate([next_token_seq, samples], axis=1)
-                        if all([len(tokenizer.events[event_names[b]]) == i for b in range(batch_size) if not end[b]]):
+                                next_token_seq = np.concatenate([next_token_seq, samples], axis=1)
+                                if all([len(tokenizer.events[event_names[b]]) == i for b in range(batch_size) if not end[b]]):
+                                    break
+ 
+                        if next_token_seq.shape[1] < max_token_seq:
+                            next_token_seq = np.pad(next_token_seq,
+                                                    ((0, 0), (0, max_token_seq - next_token_seq.shape[-1])),
+                                                    mode="constant", constant_values=tokenizer.pad_id)
+                        next_token_seq = next_token_seq[:, None, :]
+                        input_tensor = np.concatenate([input_tensor, next_token_seq], axis=1)
+                        cur_len += 1
+                        bar.update(1)
+                        yield next_token_seq[:, 0]
+                        if all(end):
                             break
-
-                if next_token_seq.shape[1] < max_token_seq:
-                    next_token_seq = np.pad(next_token_seq, 
-                                            ((0, 0), (0, max_token_seq - next_token_seq.shape[-1])), 
-                                            mode="constant", constant_values=tokenizer.pad_id)
-                next_token_seq = next_token_seq[:, None, :]
-                input_tensor = np.concatenate([input_tensor, next_token_seq], axis=1)
-                cur_len += 1
-                bar.update(1)
-                yield next_token_seq[:, 0]
-                if all(end):
-                    break
